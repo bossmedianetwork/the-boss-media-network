e1a553df6aa1920b57703e955d12e1afe5bb699f
thebossmedianetwork <148651420+bossmedianetwork@users.noreply.github.com>
2023-10-25 14:48:17 -0300

    styles improvements, design changes

--- src/common/Button/styles.ts
+++ src/common/Button/styles.ts
@@ -1,7 +1,25 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const glowRed = keyframes`
+  0% {
+    box-shadow: 0 0 5px #d83535, 0 0 10px #d83535, 0 0 15px #d83535, 0 0 20px #d83535;
+  }
+  100% {
+    box-shadow: 0 0 10px #d83535, 0 0 20px #d83535, 0 0 30px #d83535, 0 0 40px #d83535;
+  }
+`;
+
+const glowGold = keyframes`
+  0% {
+    box-shadow: 0 0 5px #DAA520, 0 0 10px #DAA520, 0 0 15px #DAA520, 0 0 20px #DAA520;
+  }
+  100% {
+    box-shadow: 0 0 10px #DAA520, 0 0 20px #DAA520, 0 0 30px #DAA520, 0 0 40px #DAA520;
+  }
+`;
 
 export const StyledButton = styled("button")<any>`
-  background: ${(p) => p.color ? `linear-gradient(45deg, #B8860B, #8B6914)` : "radial-gradient(circle at center, #fe003e 1%, #d10000 90%, #b20000 100%)"};
+  background: ${(p) => p.color ? `linear-gradient(45deg, #B8860B, #8B6914)` : "radial-gradient(circle farthest-corner at center center, #ff5252 0%, #e63939 5%, #ff2727 90%, #b20000 100%)"};
   color: #FFFFFF;
   font-size: 1rem;
   font-weight: 700;
@@ -15,13 +33,14 @@ export const StyledButton = styled("button")<any>`
   transition: all 0.3s ease-in-out;
   position: relative;
   outline: none;
-  box-shadow: 0 0 10px ${(p) => p.color ? "#B8860B" : "#da0000"},
-              0 0 20px ${(p) => p.color ? "#B8860B" : "#da0000"};
+  box-shadow: 0 0 10px #d83535, 0 0 20px #d83535;
+  animation: ${glowRed} 1.5s infinite alternate;
 
   &:hover,
   &:active,
   &:focus {
-    background: radial-gradient(circle at center, #ffd700 1%, #DAA520 90%, #8B6914 100%);
-    box-shadow: 0 0 20px #B8860B;
+    background: radial-gradient(circle farthest-corner at center center, #ffd700 1%, #DAA520 90%, #8B6914 100%);
+    box-shadow: 0 0 20px #DAA520, 0 0 30px #DAA520, 0 0 40px #DAA520;
+    animation: ${glowGold} 1.5s infinite alternate;
   }
 `;
--- src/components/MiddleBlock/styles.tsx
+++ src/components/MiddleBlock/styles.tsx
@@ -13,11 +13,22 @@ export const MiddleBlockSection = styled("section")`
 `;
 
 export const Title = styled("h6")`
-  background: -webkit-linear-gradient(right, #ffeafb, #ffeafb);
+  font-size: 2.7em;  // Adjust size as needed
+  font-weight: bold;  // Bold font for a sleek look
+  background: -webkit-linear-gradient(right, #ffffff, #ffffff);  // White background
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-  text-shadow: 0 0 5px #ffeafb, 0 0 2px #ffeafb, 0 0 2px #ffeafb, 0 0 2px #ffeafb;
+  text-shadow:
+    0 0 5px #ffffff, 
+    0 0 10px #fe0b04, 
+    0 0 15px #ff416c, 
+    0 0 18px #fe0b04, 
+    0 0 20px #fe0b04;  // Neon glow effect
+    0 0 22px #ffffff,
+  text-align: center;  // Centered text
+  margin: 0 auto;  // Centered block
 `;
+
 
 export const Content = styled("p")`
   padding: 0.75rem 0 0.75rem;
